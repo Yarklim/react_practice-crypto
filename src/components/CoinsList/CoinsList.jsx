8a188d5780fb9538a288fa9ca2aa7b54c950493b
Yaroslav Klimas <yarklim@gmail.com>
2023-09-18 13:39:24 +0300

    added api

--- src/components/CoinsList/CoinsList.jsx
+++ src/components/CoinsList/CoinsList.jsx
@@ -5,7 +5,24 @@ const CoinsList = ({ coins }) => {
   return (
     <ul className={s.coinsList}>
       {coins.map((coin) => (
-        <li key={coin.uuid}>{coin.name}</li>
+        <li className={s.coinItem} key={coin.uuid}>
+          <div className={s.coinItemInfo}>
+            <img
+              className={s.coinItemLogo}
+              src={coin.iconUrl}
+              alt={coin.name}
+            />
+            <p style={{ color: coin.color }}>{coin.name} / USD</p>
+          </div>
+          <div className={s.coinItemPrice}>
+            <p style={{ color: coin.color }}>
+              {Number(coin.price).toFixed(2)} USD
+            </p>
+            <p style={{ color: coin.color }}>
+              {Number(coin.btcPrice).toFixed(6)} BTC
+            </p>
+          </div>
+        </li>
       ))}
     </ul>
   );
